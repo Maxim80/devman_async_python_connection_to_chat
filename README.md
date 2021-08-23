53b02d4e803a7b816261c2ad84212e0886b633f1
Maxim <maxim19802104@gmail.com>
2021-08-23 16:31:41 +0300

    Add CLI options.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Подключение к чату
+
+### Установка и запуск проекта:
+```
+git clone https://github.com/Maxim80/devman_async_python_connection_to_chat.git
+pip3 install -r requirements.txt
+cd devman_async_python_connection_to_chat/
+python main.py
+```
+Необязательные аргументы:
+`--host` IP или URL адрес для подключения.
+`--port` Порт для подключения.
+`--history` Путь к файлу с историей переписки.
+`-h --help`Справочная информация.
+
+### Цель проекта
+Код написан в образовательных целях на онлайн-курсе для веб-разработчиков [dvmn.org](https://dvmn.org/).
+
+***
+# Connect to chat
+
+### Installing and running the project:
+```
+git clone https://github.com/Maxim80/devman_async_python_connection_to_chat.git
+pip3 install -r requirements.txt
+cd devman_async_python_connection_to_chat/
+python main.py
+```
+Optional Arguments:
+`--host` IP or URL address to connection.
+`--port` Connection port.
+`--history` The path to the file with the history of the correspondence.
+`-h --help` Reference Information.
+
+### Project Goals
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import asyncio
 import aiofiles
 import datetime
+import argparse
 
 
-async def chat_reader():
+async def chat_reader(args):
     while True:
         reader, writer = await asyncio.open_connection(
-            'minechat.dvmn.org',
-            5000,
+            args.host,
+            args.port,
         )
         data = await reader.readline()
         datetime_now = datetime.datetime.now().strftime('%d.%m.%Y %H:%M')
         message = f'[{datetime_now}] {data.decode()}'
-        async with aiofiles.open('log.txt', 'a') as f:
+        async with aiofiles.open(args.history, 'a') as f:
             await f.write(message)
 
         print(message)
 
 
 if __name__ == '__main__':
-    asyncio.run(chat_reader())
+    parser = argparse.ArgumentParser(description='Script to connection to chats')
+    parser.add_argument('--host', type=str, default='minechat.dvmn.org',
+        help='IP or URL hosts address for connection.')
+    parser.add_argument('--port', type=int, default=5000,
+        help='Port for connection.')
+    parser.add_argument('--history', type=str, default='./minechat.history',
+        help='Path to the file with the history of correspondence.')
+    args = parser.parse_args()
+    asyncio.run(chat_reader(args))
