60276165aec49b0c0770415545c9b3d275dc7bc6
Maxim <maxim19802104@gmail.com>
2021-08-27 18:13:18 +0300

    Setup logging.

--- sending.py
+++ sending.py
@@ -1,14 +1,21 @@
 from dotenv import load_dotenv
 import asyncio
 import argparse
+import logging
 import os
 
 
 async def sender(args):
     reader, writer = await asyncio.open_connection(args.host, args.port)
 
+    data = await reader.readline()
+    logger.debug()
+
     writer.write(f'{args.token}\n'.encode())
     await writer.drain()
+
+    data = await reader.readline()
+    logger.debug(data.decode())
 
     writer.write(f'{args.message}\n\n'.encode())
     await writer.drain()
@@ -19,6 +26,7 @@ async def sender(args):
 
 if __name__ == '__main__':
     load_dotenv()
+
     parser = argparse.ArgumentParser(description='Script to connection to chats')
     parser.add_argument('--host', type=str, default='minechat.dvmn.org',
         help='IP or URL hosts address for connection.')
@@ -26,7 +34,13 @@ if __name__ == '__main__':
         help='Port for connection.')
     parser.add_argument('--token', type=str, default=os.getenv('MINECHAT_ACCESS_TOKEN'),
         help='Minechat access token.')
+    parser.add_argument('--logger', help='Enable logger', action='store_true')
     parser.add_argument('--message', type=str, help='Message to send to chat.',
         default='Здравствуйте! Я тестирую чатик.')
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger('sender')
+    logger.disabled = not args.logger
+
     asyncio.run(sender(args))
